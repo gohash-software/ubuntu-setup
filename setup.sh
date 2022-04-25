5670cc755f2b7b7c970d50b1403abdbb4cecf1ea
Matt Williams <matt@gohash.tech>
2022-04-25 12:05:13 -0700

    :godmode: Add fancy messages, and make script more robust

--- setup.sh
+++ setup.sh
@@ -1,48 +1,114 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
+set -eu
+printf '\n'
 
-function pre_hooks() {
-	if [[ ! -d ~/Downloads/debs ]]; then
+BOLD="$(tput bold 2>/dev/null || printf '')"
+GREY="$(tput setaf 0 2>/dev/null || printf '')"
+UNDERLINE="$(tput smul 2>/dev/null || printf '')"
+RED="$(tput setaf 1 2>/dev/null || printf '')"
+GREEN="$(tput setaf 2 2>/dev/null || printf '')"
+YELLOW="$(tput setaf 3 2>/dev/null || printf '')"
+BLUE="$(tput setaf 4 2>/dev/null || printf '')"
+MAGENTA="$(tput setaf 5 2>/dev/null || printf '')"
+NO_COLOR="$(tput sgr0 2>/dev/null || printf '')"
+
+info() {
+  printf '%s\n' "${BOLD}${GREY}>${NO_COLOR} $*"
+}
+
+warn() {
+  printf '%s\n' "${YELLOW}! $*${NO_COLOR}"
+}
+
+error() {
+  printf '%s\n' "${RED}x $*${NO_COLOR}" >&2
+}
+
+completed() {
+  printf '%s\n' "${GREEN}✓${NO_COLOR} $*"
+}
+
+has() {
+  command -v "$1" 1>/dev/null 2>&1
+}
+
+# Make sure user is not using zsh or non-POSIX-mode bash, which can cause issues
+verify_shell_is_posix_or_exit() {
+  if [ -n "${ZSH_VERSION+x}" ]; then
+    error "Running installation script with \`zsh\` is known to cause errors."
+    error "Please use \`sh\` instead."
+    exit 1
+  elif [ -n "${BASH_VERSION+x}" ] && [ -z "${POSIXLY_CORRECT+x}" ]; then
+    error "Running installation script with non-POSIX \`bash\` may cause errors."
+    error "Please use \`sh\` instead."
+    exit 1
+  else
+    true  # No-op: no issues detected
+  fi
+}
+
+pre_hooks() {
+	info "Creating debs directory..."
+	if [ ! -d ~/Downloads/debs ]; then
 		mkdir -p ~/Downloads/debs
 	fi
 }
 
-function update_system() {
+update_system() {
+	info "Updating the system..."
 	sudo apt-get update && sudo apt-get upgrade -y
 }
 
-function install_standard() {
-	sudo apt install neovim fish dconf-editor gnome-tweaks apt-transport-https curl git inxi htop -y
+install_standard() {
+	info "Installing suite of standard programs..."
+	sudo apt install neovim fish dconf-editor gnome-tweaks apt-transport-https curl git inxi htop chrome-gnome-shell gnome-shell-extension-prefs -y
 }
 
 # install brave and google-chrome
-function install_browswers() {
-	# install brave
-	sudo apt install apt-transport-https curl
-	sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-	echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-	sudo apt update
-	sudo apt install brave-browser
+install_browsers() {
+	info "Installing browsers..."
+	# install brave if it is not already on the system
+	if ! has brave-browser; then
+		sudo apt install apt-transport-https curl
+		sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+		echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+		sudo apt update
+		sudo apt install brave-browser -y
+		completed "Brave is now installed"
+	else
+		info "Brave is already installed"
+	fi
 
-	# install google chrome
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	sudo dpkg -i google-chrome-stable_current_amd64.deb
-	mv -v google-chrome-stable_current_amd64.deb ~/Downloads/debs
+	# install google chrome if its not already on the system
+	if ! has google-chrome; then
+		wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+		sudo dpkg -i google-chrome-stable_current_amd64.deb
+		mv -v google-chrome-stable_current_amd64.deb ~/Downloads/debs
+		completed "Google Chrome is now installed"
+	else
+		info "Google Chrome is already installed"
+	fi
 }
 
-function install_snaps() {
+install_snaps() {
+	info "Installing 3rd party software via snap packages..."
 	snap install discord spotify bitwarden onlyoffice-desktopeditors
 }
 
-function uninstall_useless_programs() {
-	sudo apt remove --purge -y libreoffice-* rhythmbox
+uninstall_useless_programs() {
+	info "Removing programs we don't need"
+	sudo apt remove --purge -y libreoffice-* rhythmbox totem
 }
 
-pre_hooks
+verify_shell_is_posix_or_exit
 update_system
+pre_hooks
 install_standard
-install_browswers
+install_browsers
 install_snaps
-uninstall_libre_office
+uninstall_useless_programs
 
+info "Removing stale packages and libraries..."
 sudo apt autoremove
+completed "Done..."
